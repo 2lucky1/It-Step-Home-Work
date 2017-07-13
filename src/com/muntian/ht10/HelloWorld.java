adc4a6fc15df8afd38b789cf04f5fb23e2a87501
Nikolai Muntian <lanikolya1989@gmail.com>
2017-07-14 01:41:07 +0300

    Created first project on git.

---
+++ src/com/muntian/ht10/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.muntian.ht10;
+
+/**
+ *
+ * Created by Note on 14.07.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
