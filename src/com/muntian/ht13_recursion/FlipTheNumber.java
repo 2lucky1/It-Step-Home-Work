52d7ad2d44003078a3d05d020ddeafacf2cd56af
Nikolai Muntian <lanikolya1989@gmail.com>
2017-08-21 17:00:44 +0300

    I`ve optimised my app. And now it return reversed natural number.

--- src/com/muntian/ht13_recursion/FlipTheNumber.java
+++ src/com/muntian/ht13_recursion/FlipTheNumber.java
@@ -3,29 +3,51 @@ package com.muntian.ht13_recursion;
 import java.util.Scanner;
 
 public class FlipTheNumber {
-    private static String userNumber;
-    private static String reversedNumber = "";
-    private static int lastIndex;
 
     public static void main(String[] args) {
-        enterTheNumber();
-        lastIndex = userNumber.length()-1;
-        reverse(userNumber,lastIndex);
-        System.out.println(reversedNumber);
+        int index = 0;
+        int testNumber = 777888;
+        int testReversedNumber = 888777;
+        int userNumber = enterNaturalNumber();
+        System.out.println(getReversedNumber(userNumber, index));
+        test(testNumber, testReversedNumber, index);
+
     }
 
-    private static void enterTheNumber() {
-        System.out.println("Enter the number");
-        Scanner input = new Scanner(System.in);
-        userNumber = input.nextLine();
+    private static int getReversedNumber(int userNumber, int index) {
+        return Integer.parseInt(reverseOfString(userNumber, index));
     }
 
-    private static void reverse(String userNumber,int lastIndex) {
-        if (lastIndex<0){
-            return;
+    private static String convertNumberToString(int userNumber) {
+        return String.valueOf(userNumber);
+    }
+
+    private static int enterNaturalNumber() {
+        while (true) {
+            System.out.println("Enter the number");
+            Scanner input = new Scanner(System.in);
+            if (input.hasNextInt()) {
+                int userNumber = input.nextInt();
+                if (userNumber > 0) {
+                    return userNumber;
+                }
+            } else {
+                System.out.println("Input error: Enter a natural integer number");
+            }
         }
-        reversedNumber+=userNumber.charAt(lastIndex);
-        lastIndex--;
-        reverse(userNumber,lastIndex);
+    }
+
+    private static String reverseOfString(int userNumber, int index) {
+        String enterByUser = convertNumberToString(userNumber);
+        return (index == enterByUser.length()) ? "" : reverseOfString(userNumber, index + 1) + enterByUser.charAt(index);
+    }
+
+    private static void test(int testNumber, int testReversedNumber, int index) {
+        if (getReversedNumber(testNumber, index) == testReversedNumber) {
+            System.out.println("Test passed!");
+        } else {
+            System.out.println("The test is failed!");
+        }
+
     }
 }
