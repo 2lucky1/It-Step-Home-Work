9fa3a3176342927641e1e4fa1371c4f539d3ff22
Nikolai Muntian <lanikolya1989@gmail.com>
2017-08-27 21:35:37 +0300

    Chess completed

--- src/com/muntian/ht15_Exercise_3_34/Chess.java
+++ src/com/muntian/ht15_Exercise_3_34/Chess.java
@@ -2,24 +2,135 @@ package com.muntian.ht15_Exercise_3_34;
 
 import java.util.Scanner;
 
+import static java.lang.String.valueOf;
+
 public class Chess {
+    private static final int VERTICAL_OF_FIGURE = 0;
+    private static final int HORIZONTAL_OF_FIGURE = 1;
+    private static final int VERTICAL_OF_TARGET = 2;
+    private static final int HORIZONTAL_OF_TARGET = 3;
+    private static final int FIGURE_NUMBER = 4;
 
     public static void main(String[] args) {
-        System.out.println("Input of the vertical coordinate of figure:");
-        int a = oneCoordinate();
-        System.out.println("Input of the horizontal coordinate of figure:");
-        int b = oneCoordinate();
-        System.out.println("Input of the vertical coordinate of the target:");
-        int c = oneCoordinate();
-        System.out.println("Input of the horizontal coordinate of the target:");
-        int d = oneCoordinate();
-//        checkOfInputData();
-        targetCanBeAttacked(targetInTheAreaOfVisibility(selectChessFigure(a, b), a, b, c, d));
+        //Create the input data array
+        int[] coordinates = new int[5];
+
+        // Enter of input data
+        enterOfInputData(coordinates);
+
+        // Determine whether the selected figure threatens the specified field
+        printOfResult(targetInTheAreaOfVisibility(coordinates));
+
+        //Visualization of chess
+        visualization(coordinates);
+
+        //Test
+        printTest(test(coordinates));
 
 
     }
 
-    private static int selectChessFigure(int a, int b) {
+    private static void visualization(int[] coordinates) {
+        String[][] chessBoard = new String[10][8];
+        //Filling of chess board by white and black squares
+        for (int i = 0; i < chessBoard.length; i += 2) {
+            for (int j = 0; j < chessBoard[i].length; j += 2) {
+                chessBoard[i][j] = "W";
+                chessBoard[i][j + 1] = "B";
+            }
+        }
+
+        for (int i = 1; i < chessBoard.length; i += 2) {
+            for (int j = 0; j < chessBoard[i].length; j += 2) {
+                chessBoard[i][j] = "B";
+                chessBoard[i][j + 1] = "W";
+            }
+        }
+
+        //Horizontal numbering
+        for (int i = chessBoard.length - 1; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                chessBoard[i][j] = valueOf(j + 1);
+                chessBoard[i - 1][j] = "-";
+            }
+        }
+
+        //Specifying the figure and the target on the board
+        chessBoard[coordinates[VERTICAL_OF_FIGURE] - 1][coordinates[HORIZONTAL_OF_FIGURE] - 1] = "f";
+        chessBoard[coordinates[VERTICAL_OF_TARGET] - 1][coordinates[HORIZONTAL_OF_TARGET] - 1] = "t";
+
+        //Display chess board
+        for (int i = 0; i < chessBoard.length; i++) {
+            if (i < 8) {
+                System.out.print((chessBoard.length - (i + 2)) + "| ");
+            } else {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                System.out.print(chessBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printTest(boolean test) {
+        if (test) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+
+    private static boolean test(int[] coordinates) {
+        coordinates[VERTICAL_OF_FIGURE] = 2;
+        coordinates[HORIZONTAL_OF_FIGURE] = 2;
+        coordinates[VERTICAL_OF_TARGET] = 4;
+        coordinates[HORIZONTAL_OF_TARGET] = 2;
+        coordinates[FIGURE_NUMBER] = 1;
+        boolean expectedResult = true;
+        if (expectedResult == targetInTheAreaOfVisibility(coordinates)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static void printOfResult(boolean result) {
+        if (result) {
+            System.out.println("Target can be attacked");
+        } else {
+            System.out.println("The target can`t be attacked");
+        }
+    }
+
+    private static void enterOfInputData(int[] coordinates) {
+        while (true) {
+            System.out.println("Input of the vertical coordinate of figure:");
+            coordinates[VERTICAL_OF_FIGURE] = enterOfOneCoordinate();
+            System.out.println("Input of the horizontal coordinate of figure:");
+            coordinates[HORIZONTAL_OF_FIGURE] = enterOfOneCoordinate();
+            System.out.println("Input of the vertical coordinate of the target:");
+            coordinates[VERTICAL_OF_TARGET] = enterOfOneCoordinate();
+            System.out.println("Input of the horizontal coordinate of the target:");
+            coordinates[HORIZONTAL_OF_TARGET] = enterOfOneCoordinate();
+            if (checkOfCorrectEnter(coordinates)) {
+                break;
+            }
+        }
+        selectChessFigure(coordinates);
+    }
+
+    private static boolean checkOfCorrectEnter(int[] coordinates) {
+        if (coordinates[VERTICAL_OF_FIGURE] == coordinates[VERTICAL_OF_TARGET] &&
+                coordinates[HORIZONTAL_OF_FIGURE] == coordinates[HORIZONTAL_OF_TARGET]) {
+            System.out.println("Input error: Identical figure and target coordinates! Try again");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private static void selectChessFigure(int[] coordinates) {
         System.out.println("Please, choose the figure:");
         System.out.println("Rook              1");
         System.out.println("Elephant          2");
@@ -32,103 +143,65 @@ public class Chess {
             System.out.println("Enter the number of figure from 1 to 7");
             Scanner input = new Scanner(System.in);
             if (input.hasNextInt()) {
-                int userNumber = input.nextInt();
-                if (userNumber > 0 && userNumber < 8) {
-                    return userNumber;
+                coordinates[FIGURE_NUMBER] = input.nextInt();
+                if (coordinates[FIGURE_NUMBER] > 0 && coordinates[FIGURE_NUMBER] < 8) {
+                    switch (coordinates[FIGURE_NUMBER]) {
+                        case 1:
+                            System.out.println("You have chosen a rook");
+                            return;
+                        case 2:
+                            System.out.println("You have chosen an elephant");
+                            return;
+                        case 3:
+                            System.out.println("You have chosen a king");
+                            return;
+                        case 4:
+                            System.out.println("You have chosen a queen");
+                            return;
+                        case 5:
+                            System.out.println("You have chosen a white paw");
+                            return;
+                        case 6:
+                            System.out.println("You have chosen a black paw");
+                            return;
+                        case 7:
+                            System.out.println("You have chosen a black paw");
+                            return;
+                        default:
+                            System.out.println("Input error: Enter integer number from 1 to 8");
+                    }
                 }
-            } else {
-                System.out.println("Input error: Enter integer number from 1 to 8");
             }
         }
     }
 
-    private static boolean targetInTheAreaOfVisibility(int numberOfFigure, int a, int b, int c, int d) {
-        switch (numberOfFigure) {
+    private static boolean targetInTheAreaOfVisibility(int[] coordinates) {
+        //Assign short names for variables for code compactness
+        int a = coordinates[VERTICAL_OF_FIGURE];
+        int b = coordinates[HORIZONTAL_OF_FIGURE];
+        int c = coordinates[VERTICAL_OF_TARGET];
+        int d = coordinates[HORIZONTAL_OF_TARGET];
+        switch (coordinates[FIGURE_NUMBER]) {
             case 1:
-                if (a == c || b == d) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return (a == c || b == d);
             case 2:
-                System.out.println(getElephantVisibility(a,b));
-                break;
+                return (a - c == b - d);
             case 3:
-                break;
+                return (a - c <= 1 && a - c >= -1 && b - d <= 1 && b - d >= -1);
             case 4:
-                break;
+                return ((a == c || b == d) || (a - c == b - d));
             case 5:
-                break;
+                return (c - a == 1 && (b - d == -1 || b - d == 1));
             case 6:
-                break;
+                return (a - c == 1 && (b - d == -1 || b - d == 1));
             case 7:
-                break;
+                return ((c == a + 2 && ((d == b + 1) || (d == b - 1))) || (c == a + 1 && ((d == b + 2) || (d == b - 2))) || (c == a - 2 && ((d == b + 1) || (d == b - 1))) || (c == a - 1 && ((d == b + 2) || (d == b - 2))));
         }
-        System.out.println("dont work");
+        System.out.println("don`t work2" + coordinates[FIGURE_NUMBER]);
         return false;
     }
 
-    private static boolean[][]getElephantVisibility(int a, int b){
-        boolean[][] elephantVisibility = new boolean[8][8];
-        int a1 = 8 - a;
-        int b1 = b - 1;
-        //left and up
-        for (int i = a1; i >= 0; i--) {
-            if (b1 < 0) {
-                break;
-            }
-            elephantVisibility[i][b1] = true;
-            b1--;
-        }
-        //left and down
-        b1 = b - 1;
-        for (int i = a1; i <= 7; i++) {
-            if (b1 > 7) {
-                break;
-            }
-            elephantVisibility[i][b1] = true;
-            b1--;
-        }
-        //right and up
-        b1 = b - 1;
-        for (int i = a1; i >= 0; i--) {
-            if (b1 > 7) {
-                break;
-            }
-            elephantVisibility[i][b1] = true;
-            b1++;
-        }
-        //right and down
-        b1 = b - 1;
-        for (int i = a1; i<=7; i++){
-            if (b1<0){
-                break;
-            }
-            elephantVisibility[i][b1] = true;
-        }
-        return elephantVisibility;
-    }
-
-    private static void targetCanBeAttacked(boolean posibilityOfTheAttack) {
-        if (posibilityOfTheAttack) {
-            System.out.println("Target can be attacked");
-        } else {
-            System.out.println("The target can not be attacked");
-        }
-    }
-
-//    private static boolean[][] areaOfVisibility(int a, int b) {
-//        boolean[][] areaOfRook = new boolean[8][8];
-//        for (int i = 0; i < 8; i++) {
-//            areaOfRook[a][i] = true;
-//        }
-//        for (int i = 0; i < 8; i++) {
-//            areaOfRook[i][b] = true;
-//        }
-//        return areaOfRook;
-//    }
-
-    private static int oneCoordinate() {
+    private static int enterOfOneCoordinate() {
         while (true) {
             System.out.println("Enter the number from 1 to 8");
             Scanner input = new Scanner(System.in);
