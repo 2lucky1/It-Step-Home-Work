2ecb518b5036e2d67e93222832056c447f634b5e
Nikolai Muntian <lanikolya1989@gmail.com>
2017-09-01 21:15:57 +0300

    Start of grant calculation

---
+++ src/com/muntian/ht18_Grant/AccrualOfScholarships.java
@@ -0,0 +1,8 @@
+package com.muntian.ht18_Grant;
+
+public class AccrualOfScholarships {
+    public static void main(String[] args) {
+        Student nick = new Student("Muntian", "Nikolai", "Nikolayevich", 212, new int[]{5, 5, 4, 3});
+        System.out.println(nick.getLastName());
+    }
+}
---
+++ src/com/muntian/ht18_Grant/Student.java
@@ -0,0 +1,71 @@
+package com.muntian.ht18_Grant;
+
+public class Student {
+    private String lastName;
+    private String name;
+    private String patronymic;
+    private int numberOfGroup;
+    private int[] marks = new int[5];
+
+    public Student(String lastName, String name, String patronymic, int numberOfGroup, int[] marks) {
+        this.lastName = lastName;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.numberOfGroup = numberOfGroup;
+        this.marks = marks.clone();
+    }
+
+    public Student() {
+
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getNumberOfGroup() {
+        return numberOfGroup;
+    }
+
+    public void setNumberOfGroup(int numberOfGroup) {
+        this.numberOfGroup = numberOfGroup;
+    }
+
+    public int[] getMarks() {
+        return marks.clone();
+    }
+
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
+
+    public int getNumberOfTests(int[]marks){
+        int numberOfTests = 0;
+        for (int i = 0; i<marks.length;i++){
+            if (marks[i]!=0){
+                numberOfTests++;
+            }
+        }
+        return numberOfTests;
+    }
+}
