2445e6cbc3493870a564a3a15bf8eda3c7b84c5c
Nikolai Muntian <lanikolya1989@gmail.com>
2017-09-12 21:31:14 +0300

    Change name of variable Code formalization

--- src/com/muntian/ht20_Passport/Main.java
+++ src/com/muntian/ht20_Passport/Main.java
@@ -10,13 +10,15 @@ public class Main {
         passportOfUkrainian.printPassport();
 
         //Create foreign passport
-        ForeignPassport nikolaiMuntian = new ForeignPassport("AV",850790,
+        ForeignPassport semenGorbunkov = new ForeignPassport("AV",850790,
                 "Gorbunkov","Semen","Semenovich","22.11.1978",
                 "Ukrainian");
+
         //Add visas
-        nikolaiMuntian.addVisa("USA","Touristic","09.09.2017","08.12.2017");
-        nikolaiMuntian.addVisa("Grate Britain","Work","21.07.2017","21.07.2018");
+        semenGorbunkov.addVisa("USA","Touristic","09.09.2017","08.12.2017");
+        semenGorbunkov.addVisa("Grate Britain","Work","21.07.2017","21.07.2018");
+
         //Print foreign passport
-        nikolaiMuntian.printPassport();
+        semenGorbunkov.printPassport();
     }
 }
