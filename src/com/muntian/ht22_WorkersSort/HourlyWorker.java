cc701a53065954d2413244c48515bf17ca61af7a
Nikolai Muntian <lanikolya1989@gmail.com>
2017-09-19 12:55:55 +0300

    Workers sort finished

---
+++ src/com/muntian/ht22_WorkersSort/HourlyWorker.java
@@ -0,0 +1,16 @@
+package com.muntian.ht22_WorkersSort;
+
+public class HourlyWorker extends Worker {
+
+    private static final double DAYS_COEFFICIENT = 20.8;
+    private static final int WORKING_HOURS = 8;
+
+    public HourlyWorker(String name, double rate) {
+        super(name, rate);
+    }
+
+    @Override
+    public double countSalary() {
+        return DAYS_COEFFICIENT * WORKING_HOURS * getRate();
+    }
+}
