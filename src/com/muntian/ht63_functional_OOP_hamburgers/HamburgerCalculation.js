1cb9f3566a8f6aae4b80dce9fff9c01878ef0f5e
Nikolai Muntian <lanikolya1989@gmail.com>
2018-02-18 00:08:14 +0200

    Try objects like parameters

--- src/com/muntian/ht63_functional_OOP_hamburgers/HamburgerCalculation.js
+++ src/com/muntian/ht63_functional_OOP_hamburgers/HamburgerCalculation.js
@@ -1,18 +1,104 @@
 //Hamburger calculation
 
-function Hamburger() {
-    const SIZE_SMALL = "small";
-    const SIZE_LARGE = "small";
-    const STUFFING_CHEESE = "cheese";
-    const STUFFING_SALAD = "salad";
-    const STUFFING_POTATO = "potato";
-    const TOPPING_MAYO = "mayo";
-    const TOPPING_SPICE = "spice";
+function Hamburger(size, stuffing) {
 
+    const SIZE_SMALL = {
+        name : "small",
+        price : 50,
+        calories : 20
+    };
+    const SIZE_LARGE = {
+        name : "large",
+        price : 50,
+        calories : 20
+    };
+    const STUFFING_CHEESE = {
+        name : "cheese",
+        price : 50,
+        calories : 20
+    };
+    const STUFFING_SALAD = {
+        name : "salad",
+        price : 50,
+        calories : 20
+    };
+    const STUFFING_POTATO = {
+        name : "potato",
+        price : 50,
+        calories : 20
+    };
+    const TOPPING_MAYO = {
+        name : "mayo",
+        price : 50,
+        calories : 20
+    };
+    const TOPPING_SPICE = {
+        name : "spice",
+        price : 50,
+        calories : 20
+    };
+
+    var sizeList = [
+      "small",
+      "large"
+    ];
+
+    var stuffingList = [
+      "cheese",
+      "salad",
+      "potato"
+    ];
+
+    var toppingList =[
+        "mayo",
+        "spice"
+    ];
     var _size;
     var _stuffing;
     var _topping;
     var _calories = 0;
+
+    function Ingredients(name, price, calories) {
+        var _name;
+        var _price;
+        var _calories;
+
+        this.getName = function () {
+            return _name;
+        };
+
+        this.getPrice = function () {
+            return _price;
+        };
+
+        this.getCalories = function () {
+            return _calories;
+        };
+    }
+
+    function Topping(name, price, calories) {
+        Ingredient.apply(this, arguments);
+    }
+
+    function Stuffing(name, price, calories) {
+        Ingredient.apply(this, arguments);
+    }
+
+    function Size(name, price, calories) {
+        Ingredient.apply(this, arguments);
+    }
+
+    function BurgerSize() {
+
+    }
+
+    var mayo = new Topping("mayo", 20, 5);
+    var spice = new Topping("spice", 15, 0);
+    var cheese = new Stuffing("cheese", 10, 20);
+    var salad = new Stuffing("salad", 20, 5);
+    var potato = new Stuffing("potato", 15, 10);
+    var smallSize = new Size("small", 50, 20);
+    var largeSize = new Size("large", 100, 40);
 
 
     /**
@@ -20,7 +106,7 @@ function Hamburger() {
      * a few additives, provided that they are different.
      */
     this.addTopping = function (topping) {
-        if(_topping.indexOf(topping)===-1){
+        if(_topping.indexOf(topping.getName())===-1){
             _topping = topping;
         }
     };
@@ -90,13 +176,30 @@ function Hamburger() {
 
 }
 
+function Ingredient() {
+}
+
 function Topping() {
-    var price;
-    var calories;
+    var _name;
+    var _price;
+    var _calories;
+
+    this.getName = function () {
+        return _name;
+    };
+
+    this.getPrice = function () {
+        return _price;
+    };
+
+    this.getCalories = function () {
+        return _calories;
+    };
 }
 
 function LargeSize() {
-    var _price = 100;
+    Topping.call(this);
+    var price = 100;
     var calories = 40;
 
 }
