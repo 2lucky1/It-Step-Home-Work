a4fdf5385208cf377e7a6b4384c97d4682870895
Nikolai Muntian <lanikolya1989@gmail.com>
2018-02-17 18:40:42 +0200

    Finish

--- src/com/muntian/ht61_regular_expressions/hw/hw.js
+++ src/com/muntian/ht61_regular_expressions/hw/hw.js
@@ -1,21 +1,31 @@
 function isUpperCaseFirstLetter(str) {
     console.log(str[0]);
-    if(str.search(/[A-ZА-Я]/) == 0){
-       return true;
-   }
-   return false;
+    return str.search(/[A-ZА-ЯЁ]/) === 0;
 }
 
 function isValidEmail(str) {
-    var re = /^[.]{1,64} + @ + \\.* + [\\.] + [a-z]{1,6}$/;
+    var re = /^([^.][a-zA-Z0-9._-]+)@([^.@])(.+)\.([a-z]{2,6})$/;
     return re.test(str);
 }
 
 function trimString(str) {
-    return str;
+    let arrStr = str.match(/\S+/ig);
+    return arrStr.join(" ");
 }
 
-function formatCurrency(num) {
-    return String(num);
+function thousandsSeparators(num) {
+    let strNum = "" + num;
+    let arrStrNum = strNum.match(/./ig);
+    let dotIdx = arrStrNum.indexOf(".");
+    if (dotIdx === -1) {
+        dotIdx = arrStrNum.length;
+    }
+    let count = 0;
+    for (let i = dotIdx; i > 0; i--) {
+        if (count % 3 === 0 && count !== 0) {
+            arrStrNum.splice(i, 0, ",");
+        }
+        count++;
+    }
+    return arrStrNum.join("");
 }
-
