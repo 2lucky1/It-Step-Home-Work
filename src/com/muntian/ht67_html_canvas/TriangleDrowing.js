d6c8ec09c883eb9014ec96b9a80b193e9928715f
Mykola Muntian <lanikolya1989@gmail.com>
2018-04-15 20:23:35 +0300

    Created task with triangle drawing

--- src/com/muntian/ht67_html_canvas/TriangleDrowing.js
+++ src/com/muntian/ht67_html_canvas/TriangleDrowing.js
@@ -7,13 +7,9 @@ example.width = 640;
 example.height = 480;
 
 let count = prompt("Enter a number of section.");
-drowComplexTriangle(count);
+drawComplexTriangle(count);
 
-function drowComplexTriangle(count) {
-
-    if(count===0){
-
-    }
+function drawComplexTriangle(count) {
 
     let center = example.width / 2;
     let marginTop = 15;
@@ -30,33 +26,33 @@ function drowComplexTriangle(count) {
 
     for (let i = 0; i < partsNumber; i++) {
         if (i === 0) {
-            drowTriangle(x1, y1, partsHeight);
+            drawTriangle(x1, y1, partsHeight);
             x1 -= indent;
             y1 += indent;
         }
 
         topBase = topBase + topBaseIncrement;
 
-        drowTrapeze(x1, y1, topBase, partsHeight);
+        drawTrapeze(x1, y1, topBase, partsHeight);
 
         x1 = x1 - partsHeight - indent;
         y1 = y1 + partsHeight + indent;
     }
 }
 
-    function drowTriangle(x1, y1, height) {
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x1 + 2 * height, y1);
-        ctx.lineTo(x1 + height, y1 - height);
-        ctx.closePath();
-        ctx.stroke();
-    }
+function drawTriangle(x1, y1, height) {
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x1 + 2 * height, y1);
+    ctx.lineTo(x1 + height, y1 - height);
+    ctx.closePath();
+    ctx.stroke();
+}
 
-    function drowTrapeze(x1, y1, topBase, height) {
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x1 + topBase, y1);
-        ctx.lineTo(x1 + topBase + height, y1 + height);
-        ctx.lineTo(x1 - height, y1 + height);
-        ctx.closePath();
-        ctx.stroke();
-    }
+function drawTrapeze(x1, y1, topBase, height) {
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x1 + topBase, y1);
+    ctx.lineTo(x1 + topBase + height, y1 + height);
+    ctx.lineTo(x1 - height, y1 + height);
+    ctx.closePath();
+    ctx.stroke();
+}
